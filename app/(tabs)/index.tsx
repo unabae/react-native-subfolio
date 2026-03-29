4c557c84769918b4c9ee2dba761653bd0ec93a5c
unabae <laurenceunabia@gmail.com>
2026-03-29 23:04:12 +0800

    clean and update code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!s
+        Welcome to Nativewind!
       </Text>
       <Link href="/sign-in">Sign In</Link>
     </View>
